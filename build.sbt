45221ad71f139da6d541c91d95fb3a5df7c3cb2d
t733389 <alucasrio@gmail.com>
2021-09-24 12:31:03 +0200

    v01 Esqueleto app

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "testSBT"
+
+version := "0.1"
+
+scalaVersion := "2.13.6"
+
---
+++ src/main/scala/playground.scala
@@ -0,0 +1,5 @@
+object Playground extends App {
+  println ("=" * 100)
+  println ("Scala up & running...")
+  println ("=" * 100)
+}
